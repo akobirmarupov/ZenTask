8a3b202fc08661998bfd56b4b144f498b3dc3bcc
AKobirMarupov <akobirdilmurodovich@gmail.com>
2026-05-29 12:30:16 +0500

    auto migrate

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+application = get_wsgi_application()
+
 # Avtomatik migrate
 from django.core.management import call_command
 call_command('migrate', '--run-syncdb')
